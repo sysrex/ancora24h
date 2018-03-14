a122f85aeff68f36f75bd34bf7b54883557f9838
Alex Kiss <akiss@Alexs-MBP.lan>
2018-03-14 20:10:54 +0000

    initial push

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Ancora24h' });
+});
+
+module.exports = router;
